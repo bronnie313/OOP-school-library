91f95478f9d6f613bd78aa62ce0621bafa22ca3b
Bisaso Ronald <bronnie313@gmail.com>
2023-07-29 12:39:36 +0300

    ADD CREATE TEACHER METHOD

--- app.rb
+++ app.rb
@@ -15,6 +15,16 @@ end
 
 def create_person
     puts "Do you want to create a student (1) or a teacher (2)? [input the number]: "
+    choice = gets.chomp.to_i
+
+    case choice
+    when 1
+        create_student
+    when 2
+        create_teacher
+    else
+        puts "Invalid choice. Please enter 1 for student or 2 for teacher"
+    end
 end
 
 def create_book
@@ -27,4 +37,40 @@ end
 
 def list_rentals_for_person(person_id)
 
+end
+
+#Method to create student
+def create_student
+    puts "Create a student:"
+    print "Age: "
+    age = gets.chomp.to_i
+
+    print "Name: "
+    name = gets.chomp
+
+    print "Has parent permission? [y/n]: "
+    parent_permission_input = gets.chomp.downcase
+
+    parent_permission = parent_permission_input == 'y'
+    
+    student = Student.new(nil, age, name, parent_permission: parent_permission)
+
+    puts "Person created successfully"
+end
+
+#method to create teacher
+def create_student
+    puts "Create Teacher:"
+    print "Age: "
+    age = gets.chomp.to_i
+
+    print "Name: "
+    name = gets.chomp
+
+    print "Specialization: "
+    specialization = gets.chomp
+
+    teacher = Teacher.new(specialization, age, name)
+
+    puts "Person created successfully!"
 end
