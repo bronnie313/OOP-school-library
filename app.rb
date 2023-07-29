ab95e0b127327d7bc1b3231602b8ae01f332dae5
Bisaso Ronald <bronnie313@gmail.com>
2023-07-29 11:58:33 +0300

    ADD APP AND MAIN FILES FOR THE APP

---
+++ app.rb
@@ -0,0 +1,30 @@
+require './person'
+require './book'
+require './rental'
+
+def list_of_books
+    puts "List of all books:"
+
+
+end
+
+def list_of_people
+    puts "List of all people (teachers and students):"
+
+end
+
+def create_person
+
+end
+
+def create_book
+
+end
+
+def create_rental
+
+end
+
+def list_rentals_for_person(person_id)
+
+end
