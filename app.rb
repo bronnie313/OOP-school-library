138f3656b5719aafde20725cf024114411d53000
Bisaso Ronald <bronnie313@gmail.com>
2023-07-29 15:00:31 +0300

    correct error messages

--- app.rb
+++ app.rb
@@ -4,112 +4,112 @@ require './teacher'
 require './book'
 require './rental'
 
-$people = []
-$books = []
+@people = []
+@books = []
 
 def list_of_books
-    puts "List of all books:"
-    $books.each do |book, index|
-        puts "#{index + 1} - Title: #{book.title}, Author: #{book.author}"
-    end
+  puts 'List of all books:'
+  @books.each do |book, index|
+    puts "#{index + 1} - Title: #{book.title}, Author: #{book.author}"
+  end
 end
 
 def list_of_people
-    puts "List of all people (teachers and students):"
-    $people.each do |person, index|
-        puts "#{index + 1} - Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
+  puts 'List of all people (teachers and students):'
+  @people.each do |person, index|
+    puts "#{index + 1} - Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+  end
 end
 
 def create_person
-    puts "Do you want to create a student (1) or a teacher (2)? [input the number]: "
-    choice = gets.chomp.to_i
+  puts 'Do you want to create a student (1) or a teacher (2)? [input the number]: '
+  choice = gets.chomp.to_i
 
-    case choice
-    when 1
-        create_student
-    when 2
-        create_teacher
-    else
-        puts "Invalid choice. Please enter 1 for student or 2 for teacher"
-    end
+  case choice
+  when 1
+    create_student
+  when 2
+    create_teacher
+  else
+    puts 'Invalid choice. Please enter 1 for student or 2 for teacher'
+  end
 end
 
 def create_book
-    puts "Create a book: "
-    print "Title: "
-    title = gets.chomp
+  puts 'Create a book: '
+  print 'Title: '
+  title = gets.chomp
 
-    print "Author: "
-    author = gets.chomp
-    
-    book = Book.new(title, author)
-    $books << book
+  print 'Author: '
+  author = gets.chomp
 
-    puts "Book created successfully"
+  book = Book.new(title, author)
+  @books << book
+
+  puts 'Book created successfully'
 end
 
 def create_rental
-    print "Select a book from the following list by number"
-    book_index = gets.chomp.to_i
-    book = $books[book_index]
+  print 'Select a book from the following list by number'
+  book_index = gets.chomp.to_i
+  book = @books[book_index]
 
-    print "Select a person from the following list by number (not id): "
-    person_id = gets.chomp.to_i
-    person = $people[person_index]
+  print 'Select a person from the following list by number (not id): '
+  person_index = gets.chomp.to_i
+  person = @people[person_index]
 
-    puts "Date: "
-    date = gets.chomp
+  puts 'Date: '
+  date = gets.chomp
 
-    rental = Rental.new(date, person, book)
+  Rental.new(date, person, book)
 
-    puts "Rental created successfully!"
+  puts 'Rental created successfully!'
 end
 
-def list_rentals_for_person(person_id)
-    puts "ID of the person: "
-    person_id = gets.chomp.to_i
+def list_rentals_for_person
+  puts 'ID of the person: '
+  person_id = gets.chomp.to_i
 
-    person = $people.find { |p| p.id == person_id }
-    person.rentals.each do |rental|
-        puts "Date: #{rental.date}, Book #{rental.book.title} by #{rental.book.author}"
-    end
+  person = @people.find { |p| p.id == person_id }
+  person.rentals.each do |rental|
+    puts "Date: #{rental.date}, Book #{rental.book.title} by #{rental.book.author}"
+  end
 end
 
-#Method to create student
+# Method to create student
 def create_student
-    puts "Create a student:"
-    print "Age: "
-    age = gets.chomp.to_i
+  puts 'Create a student:'
+  print 'Age: '
+  age = gets.chomp.to_i
 
-    print "Name: "
-    name = gets.chomp
+  print 'Name: '
+  name = gets.chomp
 
-    print "Has parent permission? [y/n]: "
-    parent_permission_input = gets.chomp.downcase
+  print 'Has parent permission? [y/n]: '
+  parent_permission_input = gets.chomp.downcase
 
-    parent_permission = parent_permission_input == 'y'
-    
-    student = Student.new(age, name, parent_permission)
-    $people << student
+  parent_permission = parent_permission_input == 'y'
 
-    puts "Person created successfully"
+  student = Student.new(age, name, parent_permission)
+  @people << student
+
+  puts 'Person created successfully'
 end
 
-#method to create teacher
+# method to create teacher
 def create_teacher
-    puts "Create Teacher:"
-    print "Age: "
-    age = gets.chomp.to_i
+  puts 'Create Teacher:'
+  print 'Age: '
+  age = gets.chomp.to_i
 
-    print "Name: "
-    name = gets.chomp
+  print 'Name: '
+  name = gets.chomp
 
-    print "Specialization: "
-    specialization = gets.chomp
+  print 'Specialization: '
+  specialization = gets.chomp
 
-    teacher = Teacher.new(specialization, age, name)
-    $people << teacher
+  teacher = Teacher.new(specialization, age, name)
+  @people << teacher
 
-    puts "Person created successfully!"
+  puts 'Person created successfully!'
 end
